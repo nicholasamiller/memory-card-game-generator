f5765b45f2b1f7ab3205c1c0b248db55ea305d9d
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2021-10-26 15:38:43 +1100

    take out card generation function to see if under static web app limit

--- MemoryCardGameGenerator.Tests/CardGenerationFunctionIntegrationTest.cs
+++ MemoryCardGameGenerator.Tests/CardGenerationFunctionIntegrationTest.cs
@@ -11,19 +11,19 @@ namespace MemoryCardGameGenerator.Tests
     public class CardGenerationFunctionIntegrationTest
     {
 
-        [TestMethod]
-        public void GenerateCards()
-        {
-            var testData = new CardDataDto() { Chinese = "太冷了", English = "some english", Pinyin = "some pinyin" };
-            var testRecord = new CardRecord() { CardDataDto = testData, Tags = new[] { "test", "meat" } };
+        //[TestMethod]
+        //public void GenerateCards()
+        //{
+        //    var testData = new CardDataDto() { Chinese = "太冷了", English = "some english", Pinyin = "some pinyin" };
+        //    var testRecord = new CardRecord() { CardDataDto = testData, Tags = new[] { "test", "meat" } };
 
-            var httpClient = new HttpClient();
-            var testRequestData = new CardsGenerationRequestDto() { Cards = new[] { testRecord },CardsPerPage = CardsPerPage.Twenty,Name = "TestGenerate" };
-            var result = Api.CardGenerationFunction.Run(testRequestData, null).Result as FileContentResult;
+        //    var httpClient = new HttpClient();
+        //    var testRequestData = new CardsGenerationRequestDto() { Cards = new[] { testRecord },CardsPerPage = CardsPerPage.Twenty,Name = "TestGenerate" };
+        //    var result = Api.CardGenerationFunction.Run(testRequestData, null).Result as FileContentResult;
             
-            var tempFileName = Path.GetTempFileName() + ".pdf";
-            File.WriteAllBytes(tempFileName, result.FileContents);
-            System.Console.WriteLine(tempFileName);
-        }
+        //    var tempFileName = Path.GetTempFileName() + ".pdf";
+        //    File.WriteAllBytes(tempFileName, result.FileContents);
+        //    System.Console.WriteLine(tempFileName);
+        //}
     }
 }
