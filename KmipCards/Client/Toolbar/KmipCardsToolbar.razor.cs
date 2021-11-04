1185e0174f0449959e5ad8c818626dc366a5c52a
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2021-11-04 21:13:17 +1100

    reworking ui

---
+++ KmipCards/Client/Toolbar/KmipCardsToolbar.razor.cs
@@ -0,0 +1,36 @@
+﻿using KmipCards.Client.Dialogs;
+using KmipCards.Client.Interfaces;
+using KmipCards.Shared;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Threading.Tasks;
+
+namespace KmipCards.Client.Toolbar
+{
+    public partial class KmipCardsToolbar
+    {
+        DialogOptions maxWidth = new DialogOptions() { MaxWidth = MaxWidth.Large, FullWidth = true };
+
+
+
+        // open add dialog
+        [Inject]
+        IDialogService DialogService { get; set; }
+
+        [Inject]
+        ICardRepository CardRepository { get; set; }
+
+        private async Task OpenAddDialog()
+        {
+            var addCardDialog = DialogService.Show<AddCharacterDialog>("Add Character",maxWidth);
+            var result = await addCardDialog.Result;
+            var cardData = result.Data as CardRecord;
+            if (cardData != null)
+            {
+                CardRepository.AddCard(cardData);
+            }
+            
+        }
+
+    }
+}
